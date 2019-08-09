91ae256abe423b20473f4d59ebbfbc4a0f666243
huangch <huangchaohui@duiba.com.cn>
2019-08-09 10:23:12 +0800

    页面访问统计查询实现

--- src/main/java/com/hx/mapper/PageVisitRecordMapper.java
+++ src/main/java/com/hx/mapper/PageVisitRecordMapper.java
@@ -1,6 +1,11 @@
 package com.hx.mapper;
 
 import com.hx.domain.PageVisitRecord;
+import com.hx.domain.PageVisitStatistics;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+import java.util.List;
 
 
 public interface PageVisitRecordMapper {
@@ -9,4 +14,5 @@ public interface PageVisitRecordMapper {
 
     int updateHxPageVisitRecord(PageVisitRecord object);
 
+    List<PageVisitStatistics> sumLatestWeekData(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
 }
